879e15f860b6a82eb8f969908f0e543477257f02
sacredsliver <sliver_@mail.ru>
2023-08-12 21:41:47 +0300

    1 домашка

---
+++ HW1/3.py
@@ -0,0 +1,19 @@
+''' 
+Программа загадывает число от 0 до 1000. Необходимо угадать число за 10 попыток. 
+Программа должна подсказывать «больше» или «меньше» после каждой попытки.
+'''
+
+from random import randint
+
+LOWER_LIMIT = 0
+UPPER_LIMIT = 1000
+num = randint(LOWER_LIMIT, UPPER_LIMIT)
+search_num = -1
+count = 10
+while search_num != num:
+    search_num = int(input("Угадайте число: "))
+    print([["Загаданное число меньше", "Загаданное число больше"][search_num < num], "Угадали!"][search_num == num])
+    count -= 1
+    if count == 0:
+        print("Попытки закончились")
+        break
